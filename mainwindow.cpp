18005356d2a2f540a7103f031c060e31c188b5a1
OMEN <user_tx@163.com>
2023-06-20 11:04:47 +0800

    增加响应日志

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDateTime>
+
+#include <QTime>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,12 +16,33 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushTry_clicked()
-{
-    ui->textInfo->setText("are you ok? ");
+QString getTime(){
+    QDateTime current_date_time = QDateTime::currentDateTime();
+    QString current_date = current_date_time.toString("yyyy-MM-dd hh:mm:ss ");
+    return current_date;
 }
 
-void MainWindow::on_menuAuto_clicked()
+void MainWindow::on_pushTry_clicked()
 {
-    ui->textInfo->setText("Menu Auto clicked!");
+    ui->textInfo->append(getTime()+"pushTry");
+}
+
+void MainWindow::on_pushRetry_clicked()
+{
+    ui->textInfo->append(getTime()+"pushRetry");
+}
+
+void MainWindow::on_actionAuto_triggered()
+{
+    ui->textInfo->append(getTime()+"actionAuto");
+}
+
+void MainWindow::on_actionEnd_triggered()
+{
+    ui->textInfo->append(getTime()+"actionAuto");
+}
+
+void MainWindow::on_actionStep_triggered()
+{
+    ui->textInfo->append(getTime()+"actionStep");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -17,7 +17,14 @@ public:
 
 private slots:
     void on_pushTry_clicked();
-    void on_menuAuto_clicked();
+
+    void on_pushRetry_clicked();
+
+    void on_actionAuto_triggered();
+
+    void on_actionEnd_triggered();
+
+    void on_actionStep_triggered();
 
 private:
     Ui::MainWindow *ui;
